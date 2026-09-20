3648d69e236502aaae438f92c8dc41febb418c90
sewmahal-lk-dev <hirusha.sewmahal.lk.dev@gmail.com>
2026-09-20 19:05:14 +0530

    Implement administration management functionality

---
+++ src/main/java/com/banking/model/Customer.java
@@ -0,0 +1,100 @@
+package com.banking.model;
+
+import java.time.LocalDate;
+
+public class Customer {
+
+    private int customerId;
+    private String name;
+    private String address;
+    private String city;
+    private String postalCode;
+    private String phone;
+    private String email;
+    private LocalDate dateOfBirth;
+    private String password;
+    private String status;
+
+    public Customer() {
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
