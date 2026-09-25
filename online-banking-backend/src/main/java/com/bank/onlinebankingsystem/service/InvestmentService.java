e3b44d5195e71f1098ca28f417fb90eb55aef20c
IT25102492 <IT25102492@my.sliit.lk>
2026-09-25 12:48:09 +0530

    Delete online-banking-backend directory

--- online-banking-backend/src/main/java/com/bank/onlinebankingsystem/service/InvestmentService.java
+++
@@ -1,9 +0,0 @@
-package com.bank.onlinebankingsystem.service;
-
-import com.bank.onlinebankingsystem.entity.Investment;
-import java.util.List;
-
-public interface InvestmentService {
-    Investment createInvestment(Investment investment);
-    List<Investment> getInvestmentsByUserId(Long userId);
-}
--- online-banking-backend/src/main/java/com/bank/onlinebankingsystem/service/ServiceRequestService.java
+++
@@ -1,9 +0,0 @@
-package com.bank.onlinebankingsystem.service;
-
-import com.bank.onlinebankingsystem.entity.ServiceRequest;
-import java.util.List;
-
-public interface ServiceRequestService {
-    ServiceRequest createRequest(ServiceRequest request);
-    List<ServiceRequest> getRequestsByUserId(Long userId);
-}
